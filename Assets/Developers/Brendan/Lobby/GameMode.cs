3ee3c20de98ef19970779442c112f63701b789da
Brendan Chen <me@bchen.dev>
2026-01-26 16:44:50 -0800

    Add GameMode.CycleNext extension method for cycling through gamemodes

--- Assets/Developers/Brendan/Lobby/GameMode.cs
+++ Assets/Developers/Brendan/Lobby/GameMode.cs
@@ -5,4 +5,16 @@ namespace Resonance.LobbySystem
         Arena,
         Polarity
     }
+
+    public static class Extensions
+    {
+        public static GameMode CycleNext(this GameMode gameMode)
+        {
+            if (gameMode == GameMode.Arena)
+            {
+                return GameMode.Polarity;
+            }
+            return GameMode.Arena;
+        }
+    }
 }
