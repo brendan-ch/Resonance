67a1573a05225e05760c4739842328969c6a06fa
Brendan Chen <me@bchen.dev>
2026-02-02 13:49:59 -0800

    Add recording of kills, damage and deaths as methods propagated up to the server

--- Assets/Scripts/MatchLogic/MatchStatBridge.cs
+++ Assets/Scripts/MatchLogic/MatchStatBridge.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using PurrNet;
@@ -23,6 +24,7 @@ namespace Resonance.Match
         #endregion 
 
         public UnityEvent<Dictionary<PlayerID, PlayerMatchStats>> OnAllStatsUpdate = new();
+        public UnityEvent<PlayerID, PlayerID> OnPlayerKill = new();
 
         private MatchStatTracker tracker_Server;
 
@@ -40,6 +42,7 @@ namespace Resonance.Match
             {
                 tracker_Server = new MatchStatTracker(assistTimeWindow, assistDamageThreshold);
                 tracker_Server.OnAllStatsUpdated += FirePlayerStatObservers;
+                tracker_Server.OnPlayerKill += FireOnKillObservers;
             }
         }
         #endregion
@@ -48,47 +51,116 @@ namespace Resonance.Match
         [ObserversRpc]
         private void FirePlayerStatObservers(Dictionary<ulong, PlayerMatchStats> allStats)
         {
-            // define and fire UI-related listeners, etc.
-            // new PackedULong();
-            // new PlayerID();
-            
             var toPropagate = new Dictionary<PlayerID, PlayerMatchStats>();
-            
+
             foreach (var (id, stats) in allStats)
             {
-                toPropagate.Add(new PlayerID(new PackedULong(id), false), stats);
+                toPropagate.Add(UlongToPlayerId(id), stats);
             }
             OnAllStatsUpdate?.Invoke(toPropagate);
+        }
+
+        [ObserversRpc]
+        private void FireOnKillObservers(ulong killer, ulong victim)
+        {
+            OnPlayerKill?.Invoke(UlongToPlayerId(killer), UlongToPlayerId(victim));
         }
         #endregion
 
         #region Kill/Death/Assist recording
-        [ServerRpc]
         public void RecordKill(GameObject killer, GameObject victim)
         {
-            // not sure if GameObject can be sent like this, if not then
-            // define another method RecordKill_Server which takes player ID instead
-
-            if (!killer.TryGetComponent(out PlayerController.PlayerController controller))
+            PlayerController.PlayerController killerController, victimController;
+            if (TryExtractPlayerControllers(killer, victim, out killerController, out victimController))
             {
                 return;
             }
+
+            if (killerController.id?.id.value is ulong killerIdPrimitive
+                && victimController.id?.id.value is ulong victimIdPrimitive)
+            {
+                RecordKill_Server(killerIdPrimitive, victimIdPrimitive);
+            }
         }
 
-        
+
+        [ServerRpc]
+        private void RecordKill_Server(ulong killer, ulong victim)
+        {
+            tracker_Server?.RecordKill(killer, victim);
+        }
+
+        public void RecordDamage(GameObject attacker, GameObject victim, float amount)
+        {
+            PlayerController.PlayerController attackerController, victimController;
+            if (TryExtractPlayerControllers(attacker, victim, out attackerController, out victimController))
+            {
+                return;
+            }
+
+            if (attackerController.id?.id.value is ulong attackerIdPrimitive
+                && victimController.id?.id.value is ulong victimIdPrimitive)
+            {
+                RecordDamage_Server(attackerIdPrimitive, victimIdPrimitive, amount);
+            }
+        }
+
+        [ServerRpc]
+        private void RecordDamage_Server(ulong attacker, ulong victim, float amount)
+        {
+            tracker_Server?.RecordDamage(attacker, victim, amount);
+        }
+
+        public void RecordDeath(GameObject victim)
+        {
+            if (!victim.TryGetComponent(out PlayerController.PlayerController controller))
+            {
+                return;
+            }
+
+            if (controller.id?.id.value is ulong idPrimitive)
+            {
+                RecordDeath_Server(idPrimitive);
+            }
+        }
+
+        [ServerRpc]
+        private void RecordDeath_Server(ulong idPrimitive)
+        {
+            tracker_Server?.RecordDeath(idPrimitive);
+        }
+
         #endregion
 
         #region Getters for clients
         [ServerRpc]
         public async Task<PlayerMatchStats> GetStats(PlayerID playerId)
         {
-            return new PlayerMatchStats {};
+            return new PlayerMatchStats { };
         }
 
         [ServerRpc]
         public async Task<Dictionary<PlayerID, PlayerMatchStats>> GetAllStats()
         {
             return new();
+        }
+        #endregion
+
+        #region Conversion helpers
+        private PlayerID UlongToPlayerId(ulong id)
+        {
+            return new PlayerID(new PackedULong(id), false);
+        }
+
+        private static bool TryExtractPlayerControllers(GameObject first, GameObject second, out PlayerController.PlayerController killerController, out PlayerController.PlayerController victimController)
+        {
+            if (!first.TryGetComponent(out killerController) || !second.TryGetComponent(out victimController))
+            {
+                victimController = null;
+                return false;
+            }
+
+            return true;
         }
         #endregion
     }
