b3b84b7e070525fdf6f6ef39027a27ae80358db1
Brendan Chen <me@bchen.dev>
2026-02-03 13:25:24 -0800

    Make PlayerStats networked and give self same ownership as _playerController instance

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -3,37 +3,37 @@ using Resonance.UI;
 using Resonance.Match;
 using UnityEngine;
 using System.Collections;
-using Resonance.Assemblies.Match;
+using PurrNet;
 
 namespace Resonance.Player
 {
-    public class PlayerStats : MonoBehaviour
+    public class PlayerStats : NetworkBehaviour
     {
         #region Inspector Fields
         [SerializeField] private float maxHealth = 100f;
         [SerializeField] private bool respawnOnDeath = true;
-        
+
         public HealthBar healthBar;
         #endregion
-        
+
         #region Properties
         public float CurrentHealth { get; private set; }
         public float MaxHealth => maxHealth;
         public bool IsDead { get; private set; }
         #endregion
-        
+
         #region Events
         public event System.Action OnPlayerDeath;
         public event System.Action OnPlayerRespawn;
         #endregion
-        
+
         #region Component References
         private PlayerState _playerState;
         private CharacterController _characterController;
         private PlayerController.PlayerController _playerController;
         private Animator _animator;
         #endregion
-        
+
         #region Damage Tracking
         private GameObject lastAttacker;
         private float lastDamageTime;
@@ -41,14 +41,22 @@ namespace Resonance.Player
 
         #region Startup
 
+        protected override void OnSpawned()
+        {
+            base.OnSpawned();
+            enabled = isOwner;
+        }
+
         public void Awake()
         {
             _playerState = GetComponent<PlayerState>();
             _characterController = GetComponent<CharacterController>();
             _playerController = GetComponent<PlayerController.PlayerController>();
+            GiveOwnership(_playerController.owner);
+
             _animator = GetComponent<Animator>();
         }
-        
+
         private void Start()
         {
             CurrentHealth = maxHealth;
@@ -57,14 +65,14 @@ namespace Resonance.Player
             {
                 healthBar.SetSliderMax(maxHealth);
             }
-            
+
             // Register with match stat tracker
             if (MatchStatBridge.Instance != null)
             {
                 MatchStatBridge.Instance.RegisterPlayer(gameObject);
             }
         }
-        
+
         private void OnDestroy()
         {
             // Unregister from match stat tracker
@@ -80,11 +88,11 @@ namespace Resonance.Player
         {
             TakeDamage(amount, null);
         }
-        
+
         public void TakeDamage(float amount, GameObject attacker)
         {
             if (IsDead) return;
-            
+
             // Track damage for assists
             if (attacker != null && attacker != gameObject && MatchStatBridge.Instance != null)
             {
@@ -92,28 +100,28 @@ namespace Resonance.Player
                 lastAttacker = attacker;
                 lastDamageTime = Time.time;
             }
-            
+
             CurrentHealth -= amount;
             CurrentHealth = Mathf.Max(0, CurrentHealth);
-            
+
             if (healthBar != null)
             {
                 healthBar.SetSlider(CurrentHealth);
             }
-            
+
             if (CurrentHealth <= 0)
             {
                 Die(attacker);
             }
         }
-        
+
         public void Heal(float amount)
         {
             if (IsDead) return;
-            
+
             CurrentHealth += amount;
             CurrentHealth = Mathf.Min(CurrentHealth, maxHealth);
-            
+
             if (healthBar != null)
             {
                 healthBar.SetSlider(CurrentHealth);
@@ -154,7 +162,7 @@ namespace Resonance.Player
             }
 
             Debug.Log($"[PlayerStats] {gameObject.name} died!");
-            
+
             // Record kill/death in match stats
             if (MatchStatBridge.Instance != null)
             {
@@ -168,7 +176,7 @@ namespace Resonance.Player
                     MatchStatBridge.Instance.RecordDeath(gameObject);
                 }
             }
-            
+
             OnPlayerDeath?.Invoke();
 
             if (respawnOnDeath)
@@ -179,9 +187,9 @@ namespace Resonance.Player
 
         private IEnumerator RespawnCoroutine()
         {
-            float respawnDelay = Resonance.Player.Respawn.Instance != null ? 
+            float respawnDelay = Resonance.Player.Respawn.Instance != null ?
                                  Resonance.Player.Respawn.Instance.RespawnDelay : 3f;
-            
+
             Debug.Log($"[PlayerStats] {gameObject.name} respawning in {respawnDelay}s");
             yield return new WaitForSeconds(respawnDelay);
             Respawn();
@@ -191,24 +199,24 @@ namespace Resonance.Player
         {
             StartCoroutine(RespawnSequence());
         }
-        
+
         private IEnumerator RespawnSequence()
         {
             IsDead = false;
-            
+
             // Clear damage tracking
             lastAttacker = null;
-            
+
             if (_playerState != null)
             {
                 _playerState.SetPlayerMovementState(PlayerMovementState.Idling);
             }
-            
+
             if (_playerController != null)
             {
                 _playerController.ResetState();
             }
-            
+
             Transform spawnPoint = Resonance.Player.Respawn.Instance?.GetSpawnPoint();
 
             if (spawnPoint != null && _characterController != null)
@@ -216,42 +224,42 @@ namespace Resonance.Player
                 transform.position = spawnPoint.position;
                 transform.rotation = spawnPoint.rotation;
             }
-            
+
             if (_characterController != null)
             {
                 if (_characterController.stepOffset <= 0)
                 {
                     _characterController.stepOffset = 0.3f;
                 }
-                
+
                 _characterController.enabled = true;
             }
-            
+
             if (_playerController != null)
             {
                 _playerController.enabled = true;
             }
-            
+
             if (_animator != null)
             {
                 _animator.enabled = true;
             }
-            
+
             yield return null;
-            
+
             if (_playerController != null)
             {
                 _playerController.IsPlayerDead = false;
             }
-            
+
             CurrentHealth = maxHealth;
             if (healthBar != null)
             {
                 healthBar.SetSlider(CurrentHealth);
             }
-            
+
             Debug.Log($"[PlayerStats] {gameObject.name} respawned!");
-            
+
             OnPlayerRespawn?.Invoke();
         }
         #endregion
