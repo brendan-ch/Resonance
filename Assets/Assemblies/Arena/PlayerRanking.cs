170dd468c5a4766749638e0de0aef3d015da013e
Brendan Chen <me@bchen.dev>
2026-02-09 09:36:07 -0800

    Remove the stored rank from PlayerRanking, and the ToString method

--- Assets/Assemblies/Arena/PlayerRanking.cs
+++ Assets/Assemblies/Arena/PlayerRanking.cs
@@ -7,11 +7,7 @@ namespace Resonance.Assemblies.Arena
     {
         public ulong player;
         public PlayerMatchStats stats;
-        public int rank;
-        
-        public override string ToString()
-        {
-            return $"Rank {rank}: {player} - {stats.ToString()}";
-        }
+
+        // Ranking is based on item index, not stored property
     }
 }
