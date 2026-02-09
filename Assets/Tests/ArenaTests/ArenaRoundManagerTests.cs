c0380639081afd828e1681b997028e75b86f8a30
Brendan Chen <me@bchen.dev>
2026-02-09 09:36:24 -0800

    Implement remaining tests for ArenaRoundManager

--- Assets/Tests/ArenaTests/ArenaRoundManagerTests.cs
+++ Assets/Tests/ArenaTests/ArenaRoundManagerTests.cs
@@ -59,26 +59,57 @@ public class ArenaRoundManagerTests
     [Test]
     public void EndMatch_FiresOnMatchEndEvent()
     {
+        roundManager.StartMatch();
 
+        ulong? capturedWinner = null;
+        int eventCallCount = 0;
+
+        roundManager.OnMatchEnd += (winner) =>
+        {
+            capturedWinner = winner;
+            eventCallCount++;
+        };
+
+        ulong expectedWinner = 1;
+        roundManager.EndMatch(expectedWinner);
+
+        Assert.AreEqual(1, eventCallCount);
+        Assert.AreEqual(expectedWinner, capturedWinner);
     }
 
     [Test]
     public void EndMatch_UpdatesTheMatchStatus()
     {
+        roundManager.StartMatch();
+        roundManager.EndMatch(1);
 
+        Assert.AreEqual(false, roundManager.IsMatchActive);
+        Assert.AreEqual(true, roundManager.IsMatchEnded);
     }
 
 
     [Test]
     public void EndMatch_AutoStartsNextRoundIfConfigured()
     {
+        var autoStartManager = new ArenaRoundManager(statTracker, autoStartNextRound: true);
+        autoStartManager.StartMatch();
+        autoStartManager.EndMatch(1);
 
+        Assert.AreEqual(true, autoStartManager.IsMatchActive);
+        Assert.AreEqual(false, autoStartManager.IsMatchEnded);
     }
 
     [Test]
     public void EndMatch_DoesNothingIfMatchNotActive()
     {
+        int eventCallCount = 0;
+        roundManager.OnMatchEnd += (_) => eventCallCount++;
 
+        roundManager.EndMatch(1);
+
+        Assert.AreEqual(0, eventCallCount);
+        Assert.AreEqual(false, roundManager.IsMatchActive);
+        Assert.AreEqual(false, roundManager.IsMatchEnded);
     }
     #endregion
 
@@ -86,28 +117,52 @@ public class ArenaRoundManagerTests
     [Test]
     public void StartMatch_UpdatesTheMatchStatus()
     {
+        roundManager.StartMatch();
 
+        Assert.AreEqual(true, roundManager.IsMatchActive);
+        Assert.AreEqual(false, roundManager.IsMatchEnded);
     }
 
 
     [Test]
     public void StartMatch_FiresOnMatchStartEvent()
     {
+        int eventCallCount = 0;
+        roundManager.OnMatchStart += () => eventCallCount++;
 
+        roundManager.StartMatch();
+
+        Assert.AreEqual(1, eventCallCount);
     }
 
     [Test]
     public void StartMatch_DoesNothingIfMatchAlreadyActive()
     {
+        roundManager.StartMatch();
 
+        int eventCallCount = 0;
+        roundManager.OnMatchStart += () => eventCallCount++;
+
+        roundManager.StartMatch();
+
+        Assert.AreEqual(0, eventCallCount);
+        Assert.AreEqual(true, roundManager.IsMatchActive);
     }
 
     [Test]
     public void StartMatch_ResetsMatchStatTracker()
     {
+        statTracker.RecordKill(1, 2);
+        statTracker.RecordKill(3, 4);
 
+        roundManager.StartMatch();
+
+        var allStats = statTracker.GetAllStats();
+        foreach (var kvp in allStats)
+        {
+            Assert.AreEqual(new PlayerMatchStats(), kvp.Value);
+        }
     }
-
 
     #endregion
 
@@ -116,7 +171,20 @@ public class ArenaRoundManagerTests
     [Test]
     public void GetLeaderboard_GetsAllPlayerRankingsDependentOnStats()
     {
+        statTracker.RecordKill(1, 2);
+        statTracker.RecordKill(1, 2);
+        statTracker.RecordKill(1, 2);
+        statTracker.RecordKill(3, 2);
+        statTracker.RecordKill(3, 2);
+        statTracker.RecordKill(4, 2);
 
+        var leaderboard = roundManager.GetLeaderboard();
+
+        Assert.AreEqual(4, leaderboard.Count);
+        Assert.AreEqual((ulong)1, leaderboard[0].player);
+        Assert.AreEqual((ulong)3, leaderboard[1].player);
+        Assert.AreEqual((ulong)4, leaderboard[2].player);
+        Assert.AreEqual((ulong)2, leaderboard[3].player);
     }
     #endregion
 }
