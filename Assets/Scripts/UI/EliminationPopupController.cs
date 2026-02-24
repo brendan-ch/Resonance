f0dc35cd8d75a9269a8c3e1172b666804c3b4141
Savana Chou <sachou@chapman.edu>
2026-02-23 23:52:35 -0800

    changed elimination pop up to use MVVM

--- Assets/Scripts/UI/EliminationPopupController.cs
+++ Assets/Scripts/UI/EliminationPopupController.cs
@@ -1,43 +1,52 @@
 using UnityEngine;
 using System.Collections;
-using PurrNet;
-using Resonance.Match;
 
 public class EliminationPopupController : MonoBehaviour
 {
     [SerializeField] private GameObject popupRoot;
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private float fadeDuration = 0.15f;
-    [SerializeField] private float displayTime = 0.6f;
+    [SerializeField] private float displayTime = 0.25f;
 
-    private MatchStatNetworkAdapter matchStats;
+    private PlayerViewModel viewModel;
+    
+    private void Start()
+    {
+        StartCoroutine(WaitForViewModel());
+    }
 
+    private IEnumerator WaitForViewModel()
+    {
+        while (viewModel == null)
+        {
+            viewModel = FindObjectOfType<PlayerViewModel>();
+            yield return null;
+        }
+
+        viewModel.GotKill.ChangeEvent += OnGotKill;
+    }
+    
     private void Awake()
     {
-        popupRoot.SetActive(true); // keep active so CanvasGroup works
+        popupRoot.SetActive(true);
         canvasGroup.alpha = 0f;
-
-        matchStats = MatchLogicNetworkAdapter.Instance?.MatchStats;
     }
 
     private void OnEnable()
     {
-        if (matchStats != null)
-            matchStats.OnPlayerKill += HandlePlayerKill;
+        if (viewModel != null)
+            viewModel.GotKill.ChangeEvent += OnGotKill;
     }
 
     private void OnDisable()
     {
-        if (matchStats != null)
-            matchStats.OnPlayerKill -= HandlePlayerKill;
+        if (viewModel != null)
+            viewModel.GotKill.ChangeEvent -= OnGotKill;
     }
 
-    private void HandlePlayerKill(PlayerID killer, PlayerID victim)
+    private void OnGotKill(bool value)
     {
-        if (killer == NetworkManager.main.localPlayer)
-        {
-            ShowPopup();
-        }
+        if (value) ShowPopup();
     }
 
     private void ShowPopup()
@@ -48,28 +57,20 @@ public class EliminationPopupController : MonoBehaviour
 
     private IEnumerator FadeRoutine()
     {
-        // Fade In
         yield return Fade(0f, 1f, fadeDuration);
-
-        // Hold
         yield return new WaitForSeconds(displayTime);
-
-        // Fade Out
         yield return Fade(1f, 0f, fadeDuration);
     }
 
     private IEnumerator Fade(float start, float end, float duration)
     {
         float time = 0f;
-
         while (time < duration)
         {
             time += Time.deltaTime;
-            float t = time / duration;
-            canvasGroup.alpha = Mathf.Lerp(start, end, t);
+            canvasGroup.alpha = Mathf.Lerp(start, end, time / duration);
             yield return null;
         }
-
         canvasGroup.alpha = end;
     }
 }
