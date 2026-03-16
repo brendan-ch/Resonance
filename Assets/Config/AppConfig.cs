01b5fe2ea5d009d88ea17bd59e6033982e055cb2
Brendan Chen <me@bchen.dev>
2026-03-15 19:21:17 -0700

    Reorganize build scripts into Resonance.BuildTools namespace

--- Assets/Config/AppConfig.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AppConfig", menuName = "Resonance/Build Configuration")]
-public class AppConfig : ScriptableObject
-{
-    public bool enableSteamLobby;
-    public bool useProductionRelay;
-    public bool isProduction;
-}
